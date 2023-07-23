e3aec0458f427b7c1551d65682cc488360396b82
Rita Moura <ritamoura520@gmail.com>
2023-07-23 18:40:22 -0300

    Feat: adiciona a escolha da cor ao jogo.

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
-window.addEventListener('load', () => {
-  const colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'pink', 'cyan', 'magenta'];
+// main.js
+const circles = document.querySelectorAll('.ball');
+const answerElement = document.getElementById('answer');
 
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+function getRandomColorRGB() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+let targetColorRGB;
+
+function checkColor(event) {
+  const selectedColor = event.target.style.backgroundColor; // Obter a cor selecionada pelo usuário
+
+  if (selectedColor === targetColorRGB) {
+    answerElement.innerText = 'Acertou!';
+  } else {
+    answerElement.innerText = 'Errou! Tente novamente!';
+  }
+}
+
+function initializeGame() {
+  targetColorRGB = getRandomColorRGB(); // Gerar a cor de destino
+  document.getElementById('rgb-color').innerText = targetColorRGB; // Mostrar a cor de destino no parágrafo com o ID 'rgb-color'
+
+  const availableColors = Array.from(circles).map((circle) => circle.style.backgroundColor);
+
+  if (!availableColors.includes(targetColorRGB)) {
+    const randomCircleIndex = Math.floor(Math.random() * circles.length);
+    circles[randomCircleIndex].style.backgroundColor = targetColorRGB;
   }
 
-  function getRandomColor() {
-    const index = getRandomInt(0, colors.length);
-    return colors[index];
-  }
+  circles.forEach((circle) => {
+    if (circle.style.backgroundColor !== targetColorRGB) {
+      const randomColor = getRandomColorRGB();
+      circle.style.backgroundColor = randomColor;
+    }
+    circle.addEventListener('click', checkColor);
+  });
+  answerElement.innerText = 'Escolha uma cor';
+}
 
-  for (let index = 1; index < 7; index += 1) {
-    document.getElementById(`circle${index}`).style.backgroundColor = getRandomColor();
-  }
-});
+initializeGame();
