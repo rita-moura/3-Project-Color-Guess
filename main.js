3486db258f5e5bc0d074ca0cde1d0890772dd814
Rita Moura <ritamoura520@gmail.com>
2023-07-23 19:01:56 -0300

    Feat: adiciona placar.

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-// main.js
 const circles = document.querySelectorAll('.ball');
 const answerElement = document.getElementById('answer');
 const resetButton = document.getElementById('reset-game');
+const scoreElement = document.getElementById('score');
+
+let targetColorRGB;
+let score = 0;
 
 function getRandomColorRGB() {
   const r = Math.floor(Math.random() * 256);
@@ -10,13 +13,17 @@ function getRandomColorRGB() {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
-let targetColorRGB;
+function updateScore() {
+  scoreElement.innerText = score; // Atualizar o texto do placar com a pontuação atual
+}
 
 function checkColor(event) {
   const selectedColor = event.target.style.backgroundColor; // Obter a cor selecionada pelo usuário
 
   if (selectedColor === targetColorRGB) {
     answerElement.innerText = 'Acertou!';
+    score += 3; // Incrementar 3 pontos para cada acerto
+    updateScore(); // Atualizar o placar
   } else {
     answerElement.innerText = 'Errou! Tente novamente!';
   }
@@ -24,22 +31,22 @@ function checkColor(event) {
 
 function initializeGame() {
   targetColorRGB = getRandomColorRGB(); // Gerar a cor de destino
-  document.getElementById('rgb-color').innerText = targetColorRGB; // Mostrar a cor de destino no parágrafo com o ID 'rgb-color'
-
+  document.getElementById('rgb-color').innerText = targetColorRGB;
   const availableColors = Array.from(circles).map((circle) => circle.style.backgroundColor);
 
   if (!availableColors.includes(targetColorRGB)) {
     const randomCircleIndex = Math.floor(Math.random() * circles.length);
     circles[randomCircleIndex].style.backgroundColor = targetColorRGB;
   }
-
   circles.forEach((circle) => {
     if (circle.style.backgroundColor !== targetColorRGB) {
+      const randomColor = getRandomColorRGB();
       // eslint-disable-next-line no-param-reassign
-      circle.style.backgroundColor = getRandomColorRGB();
+      circle.style.backgroundColor = randomColor;
     }
     circle.addEventListener('click', checkColor);
   });
+
   answerElement.innerText = 'Escolha uma cor';
 }
 
